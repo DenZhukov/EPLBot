d59ff2b0a631215f3db2ece3b53add2bece5a537
Denis Zhukov <zhukovden90@gmail.com>
2021-09-07 14:43:40 +0300

    EPLBot-0: create a repository for TelegramBot

---
+++ src/main/java/com/github/EPLBot/EpLbotApplication.java
@@ -0,0 +1,13 @@
+package com.github.EPLBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpLbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpLbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/EPLBot/EpLbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.EPLBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpLbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
