7fa07c8d0a9319d9a8851b73256e5392ce0aba6b
Denis Zhukov <denis_zhukov1@epam.com>
2021-12-27 21:50:48 +0300

    EPL-10 Refactoring

--- src/test/java/com/github/EPLBot/EpLbotApplicationTests.java
+++ src/test/java/com/github/EPLBot/EpLbotApplicationTests.java
@@ -2,7 +2,9 @@ package com.github.EPLBot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import rs.baselib.test.IgnoreTest;
 
+@IgnoreTest
 @SpringBootTest
 class EpLbotApplicationTests {
 
